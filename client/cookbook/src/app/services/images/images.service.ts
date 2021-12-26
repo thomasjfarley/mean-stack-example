e0883f35f15ce383730a35083bff67c42dbe3e33
Thomas Farley <thomasjfarley@gmail.com>
2021-12-26 12:55:39 -0700

    more tinkering

--- client/cookbook/src/app/services/images/images.service.ts
+++ client/cookbook/src/app/services/images/images.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from "@angular/common/http";
+import { HttpClient } from '@angular/common/http';
 
-@Injectable({
+@Injectable( {
   providedIn: 'root'
-})
+} )
 export class ImagesService {
 
   constructor(private httpClient: HttpClient) { }
